c810b8e0ac59355bf8a2e83cd8c2c405c94e4bc4
hrvojesesar <hrvojesesar007@gmail.com>
2023-04-30 00:34:20 +0200

    Add table and relations for category table

---
+++ resources/views/category/edit.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Edit Category</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Northwind</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ url('/') }}">Početna <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('region.index') }}">Region</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('territory.index') }}">Territory</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('products.index') }}">Product</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('shipper.index') }}">Shippers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('order.index') }}">Orders</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('customerdemographic.index') }}">Customers Demographic</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('customers.index') }}">Customers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('category.index') }}">Categories</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('suppliers.index') }}">Suppliers</a>
+            </li>
+    </div>
+</nav>
+<div class="container mt-2">
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Category</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('category.index') }}" enctype="multipart/form-data">
+                    Back</a>
+            </div>
+        </div>
+    </div>
+    @if(session('status'))
+        <div class="alert alert-success mb-1 mt-1">
+            {{ session('status') }}
+        </div>
+    @endif
+    <form action="{{ route('category.update',$category->CategoryID) }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                        <strong>Category Name:</strong>
+                        <input type="text" name="CategoryName" value="{{ $category->CategoryName }}"
+                            class="form-control" placeholder="Category Name">
+                        @error('CategoryName')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Description:</strong>
+                    <textarea class="form-control" style="height:150px" name="Description" placeholder="Description">{{ $category->Description }}</textarea>
+                    @error('Description')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                        <strong>Picture:</strong>
+                        <input type="file" name="Picture" class="form-control" placeholder="Picture">
+                        @error('Picture')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                        <img src="{{ URL::to('/') }}/images/{{ $category->Picture }}" class="img-thumbnail" width="100" />
+                        <input type="hidden" name="hidden_image" value="{{ $category->Picture }}" />
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary ml-3">Submit</button>
+        </div>
+    </form>
+</div>
+</body>
+</html>
