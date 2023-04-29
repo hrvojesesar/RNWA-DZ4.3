65c98ac93b0bdf1ec1c07e2a47efba590fe81c99
hrvojesesar <hrvojesesar007@gmail.com>
2023-04-30 00:34:40 +0200

    Add table and relations for category table

---
+++ app/Models/Category.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $table = 'categories';
+
+    protected $primaryKey = 'CategoryID';
+
+    protected $fillable = [
+        'CategoryID',
+        'CategoryName',
+        'Description',
+        'Picture',
+    ];
+
+    protected $casts = [
+        'CategoryID' => 'integer',
+        'CategoryName' => 'string',
+        'Description' => 'string',
+        'Picture' => 'string',
+    ];
+
+    public $timestamps = true;
+
+    public $incrementing = true;
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'CategoryID', 'CategoryID');
+    }
+
+    use HasFactory;
+}
